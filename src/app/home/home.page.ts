15d7413c045941a82bd9566e47f46a5d52861789
Ing. Juan Pablo de Jesus Figueroa Jaramillo <programacion@capdam.gob.mx>
2022-04-18 09:12:27 -0500

    Subiendo al repo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  resultado = '';
+  valor2: string;
+  valor1: string;
+  sumar: boolean;
+  restar: boolean;
+  multiplicar: boolean;
+  dividir: boolean;
 
   constructor() {}
 
+  Calcular() {
+    if (!!this.valor1 && !!this.valor2) {
+      let s = '';
+      if (this.sumar) {
+        const c = parseFloat(this.valor1) + parseFloat(this.valor2);
+        s = s + this.valor1 + '+' + this.valor2 + '=' + c + '<br>';
+      }
+      if (this.restar) {
+        const c = parseFloat(this.valor1) - parseFloat(this.valor2);
+        s = s + this.valor1 + '-' + this.valor2 + '=' + c + '<br>';
+      }
+      if (this.multiplicar) {
+        const c = parseFloat(this.valor1) * parseFloat(this.valor2);
+        s = s + this.valor1 + '*' + this.valor2 + '=' + c + '<br>';
+      }
+      if (this.dividir) {
+        const c = parseFloat(this.valor1) / parseFloat(this.valor2);
+        s = s + this.valor1 + '/' + this.valor2 + '=' + c + '<br>';
+      }
+      this.resultado = '<b>' + s + '</b>';
+    }
+  }
 }
